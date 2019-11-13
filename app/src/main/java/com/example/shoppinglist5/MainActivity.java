1ce3705a7fba43e5885abcf3f7c2932b3bb25808
BertVM52 <34281226+BertVM52@users.noreply.github.com>
2019-11-13 17:33:56 +0100

    Fetch json from server + refactoring

--- app/src/main/java/com/example/shoppinglist5/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist5/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.shoppinglist5;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,11 +17,19 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.*;
 import org.json.JSONException;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -70,6 +81,9 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("Clicked 'View my lists'");
                 selectList();
                 break;
+            case R.id.menuFetch:
+                System.out.println("Clicked 'Fetch data'");
+                fetchFromServer();
 
         }
         return true;
@@ -215,5 +229,81 @@ public class MainActivity extends AppCompatActivity {
 
     private String getFirstListName() {
         return (String) items.keySet().toArray()[0];
+    }
+
+
+    private void fetchFromServer() {
+        new JsonTask().execute("https://jsonplaceholder.typicode.com/todos/1");
+    }
+
+
+
+    private class JsonTask extends AsyncTask<String, String, String> { // source: https://stackoverflow.com/questions/33229869/get-json-data-from-url-using-android
+
+        ProgressDialog pd;
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            System.out.println(result);
+        }
     }
 }
